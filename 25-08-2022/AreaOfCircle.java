c86397d116c4b57ad6eba74c7247994ba918c9ea
IreneusDiengdoh <111556244+IreneusDiengdoh@users.noreply.github.com>
2022-08-31 18:37:21 +0530

    Create AreaOfCircle.java

---
+++ 25-08-2022/AreaOfCircle.java
@@ -0,0 +1,12 @@
+/*Write a java program to take radius and print area of circle*/
+package com.java;
+public class AreaOfCircle 
+{
+	public static void main(String[] args)
+	{
+		double area,r=2.14;
+			area=3.14*r*r;
+			area=3.14*2.14*2.14;
+			System.out.println("Area Of A Circle= "+area);
+	}
+}
