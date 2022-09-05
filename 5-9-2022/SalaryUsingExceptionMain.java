6706ab56a97dc202740a2d67789afa6d0d4c8503
IreneusDiengdoh <111556244+IreneusDiengdoh@users.noreply.github.com>
2022-09-05 11:51:04 +0530

    Update SalaryUsingExceptionMain.java

--- 5-9-2022/SalaryUsingExceptionMain.java
+++ 5-9-2022/SalaryUsingExceptionMain.java
@@ -1,3 +1,21 @@
+//WAP to input the salary of an employee and if the salary is not in the range of 10000 to 20000 and Invalid Answer exception should be thrown
+package com.java;
+public class SalaryUsingException 
+{
+	static void validate(double sal) throws InvalidSalException
+	{
+		if(sal<10000 || sal>20000)
+		{
+			throw new InvalidSalException("Salary should be in the Range of 10000 to 20000");
+		}
+		else
+		{
+			System.out.println("Salary= "+sal);
+		}
+	
+	}
+}
+
 package com.java;
 import java.util.*;
 public class SalaryUsingExceptionMain 
